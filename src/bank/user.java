8cc81f8aeff0df224ce3bdabed5be3b8fd4f909e
Peace0902 <102469424+Peace0902@users.noreply.github.com>
2023-06-01 11:38:56 +0700

    Update user.java

--- src/bank/user.java
+++ src/bank/user.java
@@ -1,8 +1,4 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package bank;
 
 import java.sql.ResultSet;
@@ -18,10 +14,7 @@ import java.sql.PreparedStatement;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
-/**
- *
- * @author kobinath
- */
+
 public class user extends javax.swing.JFrame {
 
     /**
