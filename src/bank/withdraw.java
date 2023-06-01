c9f0e0ea2f0c151b4ac54f3a91563ede588f0c79
Peace0902 <102469424+Peace0902@users.noreply.github.com>
2023-06-01 11:39:24 +0700

    Update withdraw.java

--- src/bank/withdraw.java
+++ src/bank/withdraw.java
@@ -1,8 +1,4 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package bank;
 
 import java.sql.Connection;
@@ -16,10 +12,6 @@ import java.util.logging.Level;
 import java.util.logging.Logger;
 import javax.swing.JOptionPane;
 
-/**
- *
- * @author kobinath
- */
 public class withdraw extends javax.swing.JInternalFrame {
 
     /**
